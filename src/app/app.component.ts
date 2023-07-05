c9a1cfef17d772ef0f84dce476286fc81555f900
ShadowCrypt97 <sergio970228@gmail.com>
2023-07-04 22:44:30 -0500

    Desarrollo desafio 2, lista estudiantes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'desafio-directivas-interpolacion';
+  alumnos = ['Juan', 'Pepito', 'Maria', 'Sara', 'Pedro', 'Roberto', 'Jean', 'Francisco', 'a', 'asd'];
 }
